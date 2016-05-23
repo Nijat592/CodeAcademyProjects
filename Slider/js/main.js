747db23278aa0e1d2103cd5c53bb73b8491577bd
Nicat <nijat.z@code.edu.az>
2016-05-23 22:41:44 +0400

    slider

---
+++ Slider/js/main.js
@@ -0,0 +1,21 @@
+﻿
+var images = document.getElementById("small");
+images.getElementsByTagName("img");
+function changeImage (shekil) {
+	shekil = window.event;
+	var targetElement = shekil.srcElement;
+	if (targetElement.tagName == 'IMG') {
+	document.getElementById("mainImage").src = targetElement.getAttribute("src");
+	}
+}
+
+function normalImg(x) {
+    x.style.width = "63px";
+    x.style.height = "50px";
+}
+
+function bigImg(x) {
+    x.style.width = "64px";
+    x.style.height = "51px";
+}
+
